4f9eb1b90c28bb251157f524854b60bbf106c914
Natasha Ferguson <59899329+nata-ferguson@users.noreply.github.com>
2021-12-26 08:44:26 -0600

    order update

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,16 +2,51 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    }
+
+    addFish = fish => {
+        // 1. take a copy of the exsisting state using spread operator
+        const fishes = { ...this.state.fishes };
+        // 2. add our new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish; 
+        // 3. set the new fishes object to state
+        this.setState({ fishes: fishes })
+    }
+
+    loadSampleFishes = () => {
+        this.setState({fishes: sampleFishes});
+    }
+
+    addToOrder = (key) => {
+        //1. take a copy of state
+        const order = { ...this.state.order }
+        //2. add to the order or update the number in the order
+        order[key] = order[key] + 1 || 1;
+        //3. call setState to update our state object
+        this.setState({ order });
+    }
+
     render() {
         return (
             <div className='catch-of-the-day'>
                 <div className='menu'>
                     <Header tagline='Fresh Seafood Market' />
+                    <ul className='fishes'>
+                        {Object.keys(this.state.fishes).map(key => (
+                            <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
+                        ))}
+                    </ul>
                 </div>
-                <Inventory />
-                <Order />
+                <Order fishes={this.state.fishes} order={this.state.order} />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+                
             </div>
         )
     }
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = event => {
+        //stop the form from submitting
+        event.preventDefault();
+        //get the text from that input
+        const storeName = this.myInput.current.value
+        //change the page to /store/whatever-they-entered
+        this.props.history.push(`/store/${storeName}`)
+    }
+
     render() {
         return (
-         <form className='store-selector'>
+         <form className='store-selector' onSubmit={this.goToStore}>
             <h2>Please Enter a Store</h2>
-            <input type='text' required placeholder='Store Name' defaultValue={getFunName()}/>
+            <input type='text' ref={this.myInput} required placeholder='Store Name' defaultValue={getFunName()}/>
             <button type='submit'>Visit Store</button>
          </form>
         )
